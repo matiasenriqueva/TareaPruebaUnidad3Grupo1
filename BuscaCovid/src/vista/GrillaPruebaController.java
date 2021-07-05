edad2ba4181bab38341a9af10a03236364c834ad
Matias Vergara <mavergara19@alumnos.utalca.cl>
2021-07-05 12:41:05 -0400

    cambio de prueba

---
+++ BuscaCovid/src/vista/GrillaPruebaController.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Matias
+ */
+public class GrillaPruebaController implements Initializable {
+
+    @FXML
+    private HBox c00;
+    @FXML
+    private ImageView i00;
+    @FXML
+    private HBox c10;
+    @FXML
+    private ImageView i10;
+    @FXML
+    private HBox c20;
+    @FXML
+    private ImageView i20;
+    @FXML
+    private HBox c01;
+    @FXML
+    private ImageView i01;
+    @FXML
+    private HBox c11;
+    @FXML
+    private ImageView i11;
+    @FXML
+    private HBox c21;
+    @FXML
+    private ImageView i21;
+    @FXML
+    private HBox c02;
+    @FXML
+    private ImageView i02;
+    @FXML
+    private HBox c12;
+    @FXML
+    private ImageView i12;
+    @FXML
+    private HBox c22;
+    @FXML
+    private ImageView i22;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void p00(MouseEvent event) {
+    }
+
+    @FXML
+    private void p10(MouseEvent event) {
+    }
+
+    @FXML
+    private void p20(MouseEvent event) {
+    }
+
+    @FXML
+    private void p01(MouseEvent event) {
+    }
+
+    @FXML
+    private void p11(MouseEvent event) {
+    }
+
+    @FXML
+    private void p21(MouseEvent event) {
+    }
+
+    @FXML
+    private void p02(MouseEvent event) {
+    }
+
+    @FXML
+    private void p12(MouseEvent event) {
+    }
+
+    @FXML
+    private void p22(MouseEvent event) {
+    }
+    
+}
